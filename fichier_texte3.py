8deed5c4ab6078fa25525df80f5626acbbb9934e
Andry Nyaina <andriniaina.2am@gmail.com>
2025-10-04 09:44:41 +0300

    maintenat en utilisant le module os

--- fichier_texte3.py
+++ fichier_texte3.py
@@ -1,10 +1,22 @@
+import os.path
+
 filename = "nom_fichier.txt"
 
-try:
+if os.path.exists(filename):
+    print("Le fichier existe")
+    f = open(filename, "r")
+    texte = f.read()
+    print(texte)
+    f.close()
+else:
+    print("Le fichier n'existe pas")
+
+
+"""try:
     f = open(filename, "r")
 except:
     print("ERREUR: Le fichier n'a pas pu ouvrir, car il n'xiste pas")
 else:
     texte = f.read()
     print(texte)
-    f.close
+    f.close"""
