c125e16884e678a613ae8c7e6567dcb2d18efaf9
Andry Nyaina <andriniaina.2am@gmail.com>
2025-10-05 15:49:57 +0300

    lire les données de plusieurs fichier excel

--- fichiers_exel.py
+++ fichiers_exel.py
@@ -1,10 +1,25 @@
 import openpyxl
 
-wb = openpyxl.load_workbook("octobre.xlsx")
-print(wb.sheetnames)
+wb1 = openpyxl.load_workbook("octobre.xlsx", data_only=True)
+wb2 = openpyxl.load_workbook("novembre.xlsx", data_only=True)
+wb3 = openpyxl.load_workbook("decembre.xlsx", data_only=True)
 
-sheet = wb['Feuil1']
-cell = sheet['D8']
+def add_data_in_wb(wb, d):
+    sheet = wb.active
 
-print(f"le nombres de ligne : {sheet.max_row}")
-print(f"le nombre des colonnes : {sheet.max_column}")
+    for row in range(2, sheet.max_row+1):
+        nom_article = sheet.cell(row, 1).value
+        if not nom_article:
+            break
+        total_vente = sheet.cell(row, 4).value
+        if d.get(nom_article):
+            d[nom_article].append(total_vente)
+        else:
+            d[nom_article] = [total_vente]
+
+donnees = {}
+add_data_in_wb(wb1, donnees)
+add_data_in_wb(wb2, donnees)
+add_data_in_wb(wb3, donnees)
+print(donnees)
+
